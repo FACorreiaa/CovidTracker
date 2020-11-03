172df62abed1e92a486eb61c985669b5d8763b2a
FAcorreiaa <fernandocorreia316@gmail.com>
2020-11-03 17:41:06 +0000

    creation of aggregates to check top daily cases

--- src/summary/controllers/countries.controller.ts
+++ src/summary/controllers/countries.controller.ts
@@ -1,27 +1,81 @@
-import { Body, CacheInterceptor, Controller, Get, Param, UseFilters, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  CacheInterceptor,
+  Controller,
+  Get,
+  Param,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AllExceptionsFilter } from 'src/validators/filter.validator';
 import { MongoExceptionFilter } from 'src/validators/mongoose.filter';
 import { CountriesService } from '../services/countries.service';
 
 @Controller('countriessummary')
 export class CountriesController {
-    constructor(private readonly countriesService: CountriesService) { }
+  constructor(private readonly countriesService: CountriesService) {}
 
-    @UseFilters(MongoExceptionFilter)
-    @UseFilters(AllExceptionsFilter)
-    @UseInterceptors(CacheInterceptor)
-    @Get()
-    async getCountrySummary(
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get()
+  async getCountrySummary() {
+    return await this.countriesService.getSummaryHistory();
+  }
 
-    ) {
-        return await this.countriesService.getSummaryHistory();
-    }
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('all')
+  async findAllCountrySummaries() {
+    return await this.countriesService.findAllCountrySummaries();
+  }
 
-    @UseFilters(MongoExceptionFilter)
-    @UseFilters(AllExceptionsFilter)
-    @UseInterceptors(CacheInterceptor)
-    @Get('all')
-    async findAllCountrySummaries() {
-        return await this.countriesService.findAllCountrySummaries();
-    }
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/new/deaths')
+  async getTopNewDeaths() {
+    return await this.countriesService.getTopNewDeaths();
+  }
+
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/new/recovered')
+  async getTopNewRecovered() {
+    return await this.countriesService.getTopNewRecovered();
+  }
+
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/new/confirmed')
+  async getTopNewConfirmed() {
+    return await this.countriesService.getTopNewConfirmed();
+  }
+
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/total/deaths')
+  async getTopTotalDeaths() {
+    return await this.countriesService.getTopTotalDeaths();
+  }
+
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/total/recovered')
+  async getTopTotalRecovered() {
+    return await this.countriesService.getTopTotalRecovered();
+  }
+
+  @UseFilters(MongoExceptionFilter)
+  @UseFilters(AllExceptionsFilter)
+  @UseInterceptors(CacheInterceptor)
+  @Get('top/total/confirmed')
+  async getTopTotalConfirmed() {
+    return await this.countriesService.getTopTotalConfirmed();
+  }
 }
--- src/summary/services/countries.service.ts
+++ src/summary/services/countries.service.ts
@@ -2,17 +2,19 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CountrySummaryDocument } from '../models/countrysummary.schema';
-import axios from 'axios'
+import axios from 'axios';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { subCountry } from '../models/subCountry.interface';
 @Injectable()
 export class CountriesService {
-    constructor(@InjectModel('CountrySummary') private readonly countrysummaryModel: Model<CountrySummaryDocument>
-    ) { }
+  constructor(
+    @InjectModel('CountrySummary')
+    private readonly countrysummaryModel: Model<CountrySummaryDocument>,
+  ) {}
 
-    private readonly logger = new Logger(CountriesService.name);
+  private readonly logger = new Logger(CountriesService.name);
 
-    /*
+  /*
     @Cron(CronExpression.EVERY_10_MINUTES)
     async getCountrySummary(id: string) {
         const result = await this.getCountrySummaryData()
@@ -29,45 +31,164 @@ export class CountriesService {
         this.logger.debug('Called every 10 HOURS');
         return await this.createCountrySummary();
     }*/
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopNewDeaths() {
+    this.logger.debug('Called every 5 HOURS');
 
-    @Cron(CronExpression.EVERY_5_HOURS)
-    async getSummaryHistory() {
-        this.logger.debug('Called every 5 HOURS');
-        const result = await this.getCountrySummaryData()
-        const { Countries } = result.data;
-        const newCountrySummary = new this.countrysummaryModel({
-            countrySummary: Countries,
-            createdAt: new Date()
-        });
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: { 'countrySummary.Date': -1, 'countrySummary.NewDeaths': -1 },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
 
-        newCountrySummary.save()
-        const res = await this.countrysummaryModel.find({}, (err, doc) => {
-            if (err) {
-                console.log('Error', err)
-                throw err
-            } else {
-                return doc
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopNewRecovered() {
+    this.logger.debug('Called every 5 HOURS');
 
-            }
-        }).sort('-countrySummary.Date').limit(1).exec()
-        const [data] = res;
-        return data;
-    }
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: {
+            'countrySummary.Date': -1,
+            'countrySummary.NewRecovered': -1,
+          },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
 
-    async findAllCountrySummaries() {
-        return this.countrysummaryModel.find();
-    }
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopNewConfirmed() {
+    this.logger.debug('Called every 5 HOURS');
 
-    async getCountrySummaryList(countries: string[]): Promise<subCountry[]> {
-        const data = await this.getSummaryHistory();
-        const { countrySummary } = data;
-        return countrySummary.filter((country) => countries.includes(country.Country))
-    }
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: {
+            'countrySummary.Date': -1,
+            'countrySummary.NewConfirmed': -1,
+          },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
 
-    async getCountrySummaryData() {
-        const result = await axios.get(`${process.env.BASE_URL}/summary`);
-        return result;
-    }
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopTotalDeaths() {
+    this.logger.debug('Called every 5 HOURS');
 
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: {
+            'countrySummary.Date': -1,
+            'countrySummary.TotalDeaths': -1,
+          },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
 
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopTotalRecovered() {
+    this.logger.debug('Called every 5 HOURS');
+
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: {
+            'countrySummary.Date': -1,
+            'countrySummary.TotalRecovered': -1,
+          },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
+
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getTopTotalConfirmed() {
+    this.logger.debug('Called every 5 HOURS');
+
+    const res = await this.countrysummaryModel
+      .aggregate([
+        { $unwind: '$countrySummary' },
+        {
+          $sort: {
+            'countrySummary.Date': -1,
+            'countrySummary.TotalConfirmed': -1,
+          },
+        },
+      ])
+      .limit(10)
+      .exec();
+    console.log(res);
+    return res;
+  }
+
+  @Cron(CronExpression.EVERY_5_HOURS)
+  async getSummaryHistory() {
+    this.logger.debug('Called every 5 HOURS');
+    const result = await this.getCountrySummaryData();
+    const { Countries } = result.data;
+    const newCountrySummary = new this.countrysummaryModel({
+      countrySummary: Countries,
+      createdAt: new Date(),
+    });
+
+    newCountrySummary.save();
+    const res = await this.countrysummaryModel
+      .find({}, (err, doc) => {
+        if (err) {
+          console.log('Error', err);
+          throw err;
+        } else {
+          return doc;
+        }
+      })
+      .sort('-countrySummary.Date')
+      .limit(1)
+      .exec();
+    const [data] = res;
+    return data;
+  }
+
+  async findAllCountrySummaries() {
+    return this.countrysummaryModel.find();
+  }
+
+  async getCountrySummaryList(countries: string[]): Promise<subCountry[]> {
+    const data = await this.getSummaryHistory();
+    const { countrySummary } = data;
+    return countrySummary.filter(country =>
+      countries.includes(country.Country),
+    );
+  }
+
+  async getCountrySummaryData() {
+    const result = await axios.get(`${process.env.BASE_URL}/summary`);
+    return result;
+  }
 }
