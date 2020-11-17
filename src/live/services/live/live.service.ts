5670380ca9b54ba1bd69bb4da4cb7e12ae8e7a42
FAcorreiaa <fernandocorreia316@gmail.com>
2020-11-17 13:12:07 +0000

    delete current workflow

--- src/live/services/live/live.service.ts
+++ src/live/services/live/live.service.ts
@@ -1,44 +1,59 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios from 'axios'
+import axios from 'axios';
 import { Model } from 'mongoose';
 import { LiveDocument } from 'src/live/models/live.schema';
 @Injectable()
 export class LiveService {
-    constructor(@InjectModel('Live') private readonly liveModel: Model<LiveDocument>) { }
+  constructor(
+    @InjectModel('Live') private readonly liveModel: Model<LiveDocument>,
+  ) {}
 
+  async findLiveDataByCountry(country: string) {
+    const data = await this.findLiveData(country);
+    const newCountrySummary = new this.liveModel();
+    newCountrySummary.name = data;
+    newCountrySummary.createdAt = new Date();
+    await newCountrySummary.save();
 
+    return await this.liveModel
+      .find({})
+      .sort([['createdAt', -1]])
+      .limit(1)
+      .exec();
+  }
 
-    async findLiveDataByCountry(country: string) {
-        const data = await this.findLiveData(country)
-        const newCountrySummary = new this.liveModel();
-        newCountrySummary.name = data;
-        newCountrySummary.createdAt = new Date();
-        await newCountrySummary.save();
+  //work in progress
+  async findLiveDataByCountryAfterDate(
+    country: string,
+    status: string,
+    date: Date,
+  ) {
+    const data = await this.findLiveDataAfterDate(country, status, date);
+    const newCountrySummary = new this.liveModel();
+    newCountrySummary.name = data;
+    newCountrySummary.createdAt = new Date();
+    await newCountrySummary.save();
 
+    return await this.liveModel
+      .find({})
+      .sort([['createdAt', -1]])
+      .limit(1)
+      .exec();
+  }
 
-        return await this.liveModel.find({}).sort([['createdAt', -1]]).limit(1).exec()
-    }
+  async findLiveData(country: string) {
+    const result = await axios.get(
+      `${process.env.BASE_URL}/live/country/${country}`,
+    );
 
-    async findLiveDataByCountryAfterDate(country: string, status: string, date: Date) {
-        const data = await this.findLiveDataAfterDate(country, status, date)
-        const newCountrySummary = new this.liveModel();
-        newCountrySummary.name = data;
-        newCountrySummary.createdAt = new Date();
-        await newCountrySummary.save();
+    return result.data;
+  }
 
-
-        return await this.liveModel.find({}).sort([['createdAt', -1]]).limit(1).exec()
-    }
-
-    async findLiveData(country: string) {
-        const result = await axios.get(`${process.env.BASE_URL}/live/country/${country}`)
-
-        return result.data
-    }
-
-    async findLiveDataAfterDate(country: string, status, date: Date) {
-        const result = await axios.get(`${process.env.BASE_URL}/live/country/${country}/status/${status}/date/${date}`)
-        return result.data
-    }
+  async findLiveDataAfterDate(country: string, status, date: Date) {
+    const result = await axios.get(
+      `${process.env.BASE_URL}/live/country/${country}/status/${status}/date/${date}`,
+    );
+    return result.data;
+  }
 }
