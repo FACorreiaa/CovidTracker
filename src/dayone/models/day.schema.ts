e01f95dc20c381f338035f6299c09ab4f661fc53
FAcorreiaa <fernandocorreia316@gmail.com>
2020-11-19 20:54:53 +0000

    delete unsed code

--- src/dayone/models/day.schema.ts
+++ src/dayone/models/day.schema.ts
@@ -1,6 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
-import { ProgressPlugin } from 'webpack';
 
 export type DayOneDocument = DayOne & Document;
 
